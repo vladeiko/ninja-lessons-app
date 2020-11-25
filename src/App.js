bfd196dc9462e97acf365d6ca9e394d8ae798184
vladeiko <bloodlad.hacker@yandex.ru>
2020-11-25 17:14:38 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: "Ryu", age: 30, belt: "black", id: 1 },
+      { name: "Yoshi", age: 20, belt: "green", id: 2 },
+      { name: "Crystal", age: 25, belt: "pink", id: 3 },
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to React!</h1>
+        <p>Nice to meet you here.</p>
+        <Ninjas ninjas={this.state.ninjas} />
+     </div>
+   );
+  }
 }
 
 export default App;
